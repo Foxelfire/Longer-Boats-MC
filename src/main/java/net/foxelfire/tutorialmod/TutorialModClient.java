f45abe4c6202b008971818692517aed8d6108658
Foxelfire <rubygoldsparrow@gmail.com>
2024-05-11 15:25:04 -0700

    Corrected the version number+license

---
+++ src/main/java/net/foxelfire/tutorialmod/TutorialModClient.java
@@ -0,0 +1,13 @@
+package net.foxelfire.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Actually write the client lol
+    }
+    
+
+}
