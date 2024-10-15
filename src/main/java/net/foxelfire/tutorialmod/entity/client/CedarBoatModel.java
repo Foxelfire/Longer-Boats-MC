3f9c88019a2cc7e34664a8ec26e5810200f94cd9
Foxelfire <rubygoldsparrow@gmail.com>
2024-10-14 19:31:14 -0700

    spooky ass shadow boat

--- src/main/java/net/foxelfire/tutorialmod/entity/client/CedarBoatModel.java
+++ src/main/java/net/foxelfire/tutorialmod/entity/client/CedarBoatModel.java
@@ -1,5 +1,7 @@
 package net.foxelfire.tutorialmod.entity.client;
 
+import net.foxelfire.tutorialmod.TutorialMod;
+
 // Made with Blockbench 4.11.1
 // Exported for Minecraft version 1.17+ for Yarn
 // Paste this class into your mod and generate all required imports
--- src/main/java/net/foxelfire/tutorialmod/entity/client/CedarBoatRenderer.java
+++ src/main/java/net/foxelfire/tutorialmod/entity/client/CedarBoatRenderer.java
@@ -2,20 +2,35 @@ package net.foxelfire.tutorialmod.entity.client;
 
 import net.foxelfire.tutorialmod.TutorialMod;
 import net.foxelfire.tutorialmod.entity.custom.CedarBoatEntity;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory.Context;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
 public class CedarBoatRenderer extends EntityRenderer<CedarBoatEntity>{
     private static final Identifier TEXTURE = new Identifier(TutorialMod.MOD_ID, "textures/entity/cedar_boat.png");
+    private CedarBoatModel<CedarBoatEntity> model;
 
     public CedarBoatRenderer(Context ctx) {
         super(ctx);
+        this.model = new CedarBoatModel<CedarBoatEntity>(ctx.getPart(ModModelLayers.CEDAR_BOAT));
+        this.shadowRadius = 0.8f;
     }
 
     @Override
     public Identifier getTexture(CedarBoatEntity var1) {
         return TEXTURE;
+    }
+
+    @Override
+    public void render(CedarBoatEntity entity, float yaw, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumers, int light){
+        matrixStack.push();
+        VertexConsumer vertexConsumer = vertexConsumers.getBuffer(model.getLayer(TEXTURE));
+        model.render(matrixStack, vertexConsumer, light, light, light, yaw, tickDelta, light);
+        matrixStack.pop();
+        super.render(entity, yaw, tickDelta, matrixStack, vertexConsumers, light);
     }
 
 }
