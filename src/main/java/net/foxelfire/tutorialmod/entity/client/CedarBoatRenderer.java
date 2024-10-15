3d6853531b2be7f8e7e6f5569e852659348ce634
Foxelfire <rubygoldsparrow@gmail.com>
2024-10-14 20:35:34 -0700

    rendering (for now) works !!

--- src/main/java/net/foxelfire/tutorialmod/entity/client/CedarBoatRenderer.java
+++ src/main/java/net/foxelfire/tutorialmod/entity/client/CedarBoatRenderer.java
@@ -2,12 +2,14 @@ package net.foxelfire.tutorialmod.entity.client;
 
 import net.foxelfire.tutorialmod.TutorialMod;
 import net.foxelfire.tutorialmod.entity.custom.CedarBoatEntity;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory.Context;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.RotationAxis;
 
 public class CedarBoatRenderer extends EntityRenderer<CedarBoatEntity>{
     private static final Identifier TEXTURE = new Identifier(TutorialMod.MOD_ID, "textures/entity/cedar_boat.png");
@@ -28,7 +30,9 @@ public class CedarBoatRenderer extends EntityRenderer<CedarBoatEntity>{
     public void render(CedarBoatEntity entity, float yaw, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumers, int light){
         matrixStack.push();
         VertexConsumer vertexConsumer = vertexConsumers.getBuffer(model.getLayer(TEXTURE));
-        model.render(matrixStack, vertexConsumer, light, light, light, yaw, tickDelta, light);
+        matrixStack.translate(0.0f, 1.44f, 0.0f);
+        matrixStack.multiply(RotationAxis.NEGATIVE_X.rotationDegrees(180.0f));
+        model.render(matrixStack, vertexConsumer, light, OverlayTexture.DEFAULT_UV, 1.0f, 1.0f, 1.0f, 1.0f);
         matrixStack.pop();
         super.render(entity, yaw, tickDelta, matrixStack, vertexConsumers, light);
     }
