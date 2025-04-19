adc52122e87fe59523a17532a5444db71eedcca5
Foxelfire <rubygoldsparrow@gmail.com>
2025-04-19 13:24:58 -0700

    the great renaming completed!

--- src/main/java/net/foxelfire/longer_boats/mixin/ItemRendererMixin.java
+++
@@ -1,9 +0,0 @@
-package net.foxelfire.longer_boats.mixin;
-// No comments, sorry future me - see disclaimer in ItemRendererAccessor if you're sure u want to remember why
-import net.minecraft.client.render.item.ItemRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ItemRenderer.class)
-public abstract class ItemRendererMixin {
-   
-}
--- src/main/java/net/foxelfire/longer_boats/mixin/ModelLoaderMixin.java
+++
@@ -1,9 +0,0 @@
-package net.foxelfire.longer_boats.mixin;
-
-import net.minecraft.client.render.model.ModelLoader;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ModelLoader.class)
-public abstract class ModelLoaderMixin {
-    
-}
